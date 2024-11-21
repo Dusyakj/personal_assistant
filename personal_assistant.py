c9f83070b60a8a9ae252afd668be5d7cc1c01abb
Khrobostov Lev <lev.khrobostov@list.ru>
2024-11-21 18:13:23 +0300

    add auxiliary functions

--- personal_assistant.py
+++ personal_assistant.py
@@ -2,3 +2,14 @@ import csv
 import json
 import datetime
 import os
+
+def load_data(file_path, default_data):
+    if not os.path.exists(file_path):
+        save_data(file_path, default_data)
+        return default_data
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def save_data(file_path, data):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
