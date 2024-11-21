2406d0cfa632fe01b0d49b9d7418d1a5cf4d982f
Khrobostov Lev <lev.khrobostov@list.ru>
2024-11-21 18:20:02 +0300

    add calc menu

--- personal_assistant.py
+++ personal_assistant.py
@@ -659,3 +659,18 @@ def finance_menu():
             break
         else:
             print('Некорректный выбор. Попробуйте снова.')
+
+
+
+def calculator_menu():
+    print('\nКалькулятор')
+    while True:
+        expression = input('Введите выражение для вычисления или "назад" для возврата: ')
+        if expression.lower() == "назад":
+            break
+        try:
+            import ast
+            result = eval(expression, {'__builtins__': None}, {})
+            print(f'Результат: {result}')
+        except Exception as e:
+            print(f'Ошибка: {e}')
